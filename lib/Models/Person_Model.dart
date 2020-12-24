b33f5476cdf49c582ccffe65c99bd3eb88f5efa0
Kevin Garcia <gkevinyamil@gmail.com>
2020-12-24 07:29:38 -0600

    Used Column Widget

--- lib/Models/Person_Model.dart
+++ lib/Models/Person_Model.dart
@@ -5,7 +5,33 @@ class PersonModel {
   final IconData iconoDerecha;
   final String nombre;
   final String descripcion;
+  final String nickName;
 
   PersonModel(
-      {this.nombre, this.descripcion, this.iconoDerecha, this.iconoIzquierda});
+      {this.nombre,
+      this.nickName,
+      this.descripcion,
+      this.iconoDerecha,
+      this.iconoIzquierda});
 }
+
+PersonModel persona1 = PersonModel(
+    nickName: 'Kevin Garcia',
+    nombre: 'Kevin Garcia',
+    descripcion: 'Ejemplo',
+    iconoDerecha: Icons.message_sharp,
+    iconoIzquierda: Icons.person_pin_circle_sharp);
+
+PersonModel persona2 = PersonModel(
+    nickName: 'Kevin Garcia',
+    nombre: 'Ejemplo 2',
+    descripcion: 'Ejemplo',
+    iconoDerecha: Icons.message_sharp,
+    iconoIzquierda: Icons.person_pin_circle_sharp);
+
+PersonModel persona3 = PersonModel(
+    nickName: 'Kevin Garcia',
+    nombre: 'Ejemplo 3',
+    descripcion: 'Ejemplo',
+    iconoDerecha: Icons.message_sharp,
+    iconoIzquierda: Icons.person_pin_circle_sharp);
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_app/Models/Person_Model.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+List<PersonModel> personas = [persona1, persona2, persona3];
+
 class MyApp extends StatelessWidget {
-  PersonModel persona1 = PersonModel(
-      nombre: 'kevin Yamil',
-      descripcion: 'Ejemplo',
-      iconDerecha: Icons.message_sharp,
-      iconoIzquierda: Icons.person_pin_circle_sharp);
+  Widget customListTitle(PersonModel person) {
+    return ListTile(
+        trailing: Icon(person.iconoDerecha),
+        leading: Icon(person.iconoIzquierda),
+        title: Text(person.nombre),
+        subtitle: Text(person.descripcion));
+  }
 
   // This widget is the root of your application.
   @override
@@ -22,25 +27,23 @@ class MyApp extends StatelessWidget {
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         home: Scaffold(
-          floatingActionButton: FloatingActionButton(
-            child: Icon(CupertinoIcons.plus_app),
-          ),
-          appBar: AppBar(
-            actions: <Widget>[
-              IconButton(icon: Icon(Icons.menu), onPressed: () {}),
-              IconButton(icon: Icon(Icons.message_rounded), onPressed: () {}),
-              IconButton(
-                  icon: Icon(Icons.image_search_outlined), onPressed: () {})
-            ],
-            title: Text('Material App By Me'),
-          ),
-          body: Container(
-              child: ListTile(
-            trailing: Icon(CupertinoIcons.mail),
-            leading: Icon(Icons.person_pin),
-            title: Text('Kevin Yamil'),
-            subtitle: Text('Trabajando con Modularización en Fluter'),
-          )),
-        ));
+            floatingActionButton: FloatingActionButton(
+              child: Icon(CupertinoIcons.plus_app),
+            ),
+            appBar: AppBar(
+              actions: <Widget>[
+                IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+                IconButton(icon: Icon(Icons.message_rounded), onPressed: () {}),
+                IconButton(
+                    icon: Icon(Icons.image_search_outlined), onPressed: () {})
+              ],
+              title: Text('Material App By Kevin'),
+            ),
+            body: Column(
+              children: <Widget>[
+                for (int i = 0; i < personas.length; i++)
+                  customListTitle(personas[i])
+              ],
+            )));
   }
 }
